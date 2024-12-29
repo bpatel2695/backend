a22b49adcc4d237f925194b1d91800ba4e1103b5
bpatel2695 <bpatel2695@gmail.com>
2024-12-28 21:59:58 -0500

    first commit

---
+++ index1.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(8080,() =>{
+    console.log("Server started on port 8080")
+})
+app.get("/", (req,res) =>{
+    res.send("Hello World")
+})
+app.get("/home", (req,res) =>{
+    res.send("This is home api")
+})
+app.get("/products", (req,res) =>{
+    let products = [
+        {
+            "name": "Product 1",
+            "price": 34
+        }
+    ]
+    // res.send("This is home api")
+    res.json(products)
+})
